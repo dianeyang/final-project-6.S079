f75ab9b45e797ffc23774c92f64280dfb91a998e
Diane Yang <dianeyang0@gmail.com>
2015-05-08 15:00:44 -0400

    YAAAAS inputting b-spline is COMPLETE

--- getBSpline.m
+++ getBSpline.m
@@ -10,15 +10,17 @@ function [curve] = getBSpline()
     % initial positions of control points
     controlPoints = [5  18 38 45 52 60 70 80 70 50 30  -8;
                     -10 18 -5 15 35 40 30 20 5 -10 -25 -38];
-    nPoints = length(controlPoints)
+    nPoints = length(controlPoints);
     
     % draw draggable control points & curve
     hold on
     for i=1:nPoints
-       handles(i) = impoint(a, controlPoints(1,i), controlPoints(2,i));
-       ids(i) = addNewPositionCallback(handles(i), @callback);
+       hPoints(i) = impoint(a, controlPoints(1,i), controlPoints(2,i));
+       ids(i) = addNewPositionCallback(hPoints(i), @callback);
     end
-    hLines = drawCurve(controlPoints);
+    [hLines, ~] = drawCurve(controlPoints);
+    
+    pause
     
     % called whenever impoints moves to new position
     function callback(pos)
@@ -35,10 +37,10 @@ function [curve] = getBSpline()
             newPos = computeNewPos(iEnd, iTan, pos);
             
             % update other endpoint to maintain straight line
-            removeNewPositionCallback(handles(iEnd), ids(iEnd));
-            handles(iEnd).setPosition(newPos);
+            removeNewPositionCallback(hPoints(iEnd), ids(iEnd));
+            hPoints(iEnd).setPosition(newPos);
             newControlPoints(:, iEnd) = newPos;
-            ids(iEnd) = addNewPositionCallback(handles(iEnd), @callback);
+            ids(iEnd) = addNewPositionCallback(hPoints(iEnd), @callback);
         elseif mod(iMoved, 3) == 1 % if dragged point is tangent point
             iEnd1 = mod(iMoved-2,nPoints)+1;
             iEnd2 = mod(iMoved+1, nPoints);
@@ -47,36 +49,36 @@ function [curve] = getBSpline()
             newPos2 = controlPoints(:,iEnd2) + d;
             
             % update endpoints to follow tangent point
-            removeNewPositionCallback(handles(iEnd1), ids(iEnd1));
-            removeNewPositionCallback(handles(iEnd2), ids(iEnd2));
-            handles(iEnd1).setPosition(newPos1);
+            removeNewPositionCallback(hPoints(iEnd1), ids(iEnd1));
+            removeNewPositionCallback(hPoints(iEnd2), ids(iEnd2));
+            hPoints(iEnd1).setPosition(newPos1);
             newControlPoints(:, iEnd1) = newPos1;
-            handles(iEnd2).setPosition(newPos2);
+            hPoints(iEnd2).setPosition(newPos2);
             newControlPoints(:, iEnd2) = newPos2;
-            ids(iEnd1) = addNewPositionCallback(handles(iEnd1), @callback);
-            ids(iEnd2) = addNewPositionCallback(handles(iEnd2), @callback);
+            ids(iEnd1) = addNewPositionCallback(hPoints(iEnd1), @callback);
+            ids(iEnd2) = addNewPositionCallback(hPoints(iEnd2), @callback);
         else
             iEnd = mod(iMoved-3,nPoints)+1;
             iTan = mod(iMoved-2,nPoints)+1;
             newPos = computeNewPos(iEnd, iTan, pos);
             
             % update other endpoint to maintain straight line
-            removeNewPositionCallback(handles(iEnd), ids(iEnd));
-            handles(iEnd).setPosition(newPos);
+            removeNewPositionCallback(hPoints(iEnd), ids(iEnd));
+            hPoints(iEnd).setPosition(newPos);
             newControlPoints(:, iEnd) = newPos;
-            ids(iEnd) = addNewPositionCallback(handles(iEnd), @callback);
+            ids(iEnd) = addNewPositionCallback(hPoints(iEnd), @callback);
         end
         
         % clear old lines
         clearCanvas();
 
         controlPoints = newControlPoints;
-        drawCurve(controlPoints);
+        [~, curve] = drawCurve(controlPoints);
     end
 
     function [newControlPoints] = getNewControlPoints()
         for i=1:nPoints
-            handle = handles(i);
+            handle = hPoints(i);
             newPos = handle.getPosition();
             newControlPoints(1,i) = newPos(1);
             newControlPoints(2,i) = newPos(2);
@@ -86,10 +88,7 @@ function [curve] = getBSpline()
     function [i] = getMovedPointIndex(pos, newControlPoints)
         i = 1;
         while i <= nPoints
-            sameX = (newControlPoints(1,i) == pos(1));
-            sameY = (newControlPoints(2,i) == pos(2));
-            if sameX && sameY
-                disp(i)
+            if newControlPoints(:,i) == pos';
                 break;
             end
             i = i+1;
@@ -112,19 +111,22 @@ function [curve] = getBSpline()
     end
 end
 
-function [hLines] = drawCurve(p)
-    hLines(1) = drawSpline(p(:,1), p(:,2), p(:,3), p(:,4));
-    hLines(2) = drawSpline(p(:,4), p(:,5), p(:,6), p(:,7));
-    hLines(3) = drawSpline(p(:,7), p(:,8), p(:,9), p(:,10));
-    hLines(4) = drawSpline(p(:,10), p(:,11), p(:,12), p(:,1));
+function [hLines, pts] = drawCurve(p)
+    [h1, pts1] = drawSpline(p(:,1), p(:,2), p(:,3), p(:,4));
+    [h2, pts2] = drawSpline(p(:,4), p(:,5), p(:,6), p(:,7));
+    [h3, pts3] = drawSpline(p(:,7), p(:,8), p(:,9), p(:,10));
+    [h4, pts4] = drawSpline(p(:,10), p(:,11), p(:,12), p(:,1));
     
-    hLines(5) = drawTangent(p(:,12), p(:,1), p(:,2));
-    hLines(6) = drawTangent(p(:,3), p(:,4), p(:,5));
-    hLines(7) = drawTangent(p(:,6), p(:,7), p(:,8));
-    hLines(8) = drawTangent(p(:,9), p(:,10), p(:,11));
+    h5 = drawTangent(p(:,12), p(:,1), p(:,2));
+    h6 = drawTangent(p(:,3), p(:,4), p(:,5));
+    h7 = drawTangent(p(:,6), p(:,7), p(:,8));
+    h8 = drawTangent(p(:,9), p(:,10), p(:,11));
+    
+    hLines = [h1 h2 h3 h4 h5 h6 h7 h8];
+    pts = [pts1 pts2 pts3 pts4];
 end
 
-function [hLine] = drawSpline(pt1, pt2, pt3, pt4)
+function [hLine, pts] = drawSpline(pt1, pt2, pt3, pt4)
     t = linspace(0,1,101);
     pts = kron((1-t).^3, pt1) + kron(3*(1-t).^2.*t, pt2) + kron(3*(1-t).*t.^2, pt3) + kron(t.^3, pt4);
     hLine = plot(pts(1,:),pts(2,:), 'black');

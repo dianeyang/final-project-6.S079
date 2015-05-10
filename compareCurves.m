8c0f398125d554d845755b71565a5b226fd6d8f6
Diane Yang <dianeyang0@gmail.com>
2015-05-10 17:21:34 -0400

    updated fitness function to handle rotations and scales

--- compareCurves.m
+++ compareCurves.m
@@ -1,23 +1,40 @@
-function [distance] = compareCurves(x1, y1, x2, y2)
+function [distance] = compareCurves(curve1, curve2)
 % Calculate the distance metric between 2 input curves
-
     % Compute arc lengths and normalize
-    [arcLen1, totalLen1] = arcLength(x1, y1);
-    [arcLen2, totalLen2] = arcLength(x2, y2);
+    [arcLen1, totalLen1] = arcLength(curve1);
+    [arcLen2, totalLen2] = arcLength(curve2);
     arcLen1 = arcLen1 / totalLen1;
     arcLen2 = arcLen2 / totalLen2;
 
     % Compute curvature at each point
-    curvature1 = curvature(x1, y1);
-    curvature2 = curvature(x2, y2);
+    curvature1 = curvature(curve1);
+    curvature2 = curvature(curve2);
     
-    % Compute distance between signature curves
-    distance = discreteFrechetDist([arcLen1 curvature1], [arcLen2 curvature2])
+    % Find rotation of curve that minimizes distance
+    minDist = discreteFrechetDist([arcLen1 curvature1], [arcLen2 curvature2]);
+    shift = 0;
+    for i=1:length(curvature1)
+        curvature2 = circshift(curvature2,1,2);
+        distance = discreteFrechetDist([arcLen1 curvature1], [arcLen2 curvature2])
+        if distance < minDist
+            minDist = distance;
+            shift = i;
+        end
+    end       
+    curvature2 = circshift(curvature2,shift,2);
     
-    plotTwo(x1, y1, x2, y2, curvature1, curvature2, arcLen1, arcLen2)
+    % Scale: scale signature curve
+    curvature2 = curvature2 * (totalLen2/totalLen1);
+    
+    plotTwo(curve1, curve2, curvature1, curvature2, arcLen1, arcLen2)
 end
 
-function [] = plotTwo(x1, y1, x2, y2, curv1, curv2, s1, s2)
+function [] = plotTwo(curve1, curve2, k1, k2, s1, s2)
+    x1 = curve1(1,:);
+    y1 = curve1(2,:);
+    x2 = curve2(1,:);
+    y2 = curve2(2,:);
+    
     figure
     subplot(2,1,1);
     plot(x1,y1)
@@ -28,17 +45,19 @@ function [] = plotTwo(x1, y1, x2, y2, curv1, curv2, s1, s2)
     ylabel('y')
     
     subplot(2,1,2);
-    plot(s2, curv1)
+    plot(s2, k1)
     hold on
-    plot(s2, curv2)
+    plot(s2, k2)
     title('Signature curve')
     xlabel('Arc length')
     ylabel('Curvature')
 end
 
-function [curvature] = curvature(x, y)
+function [curvature] = curvature(curve)
 % Compute curvature = u_xx / (1 + u_x^2)^(3/2)
 % Can also be viewed as the 2nd derivative of slope wrt arc length
+    x = curve(1,:);
+    y = curve(2,:);
     x_prime = gradient(x);
     y_prime = gradient(y);
     x_2prime = gradient(x_prime);
@@ -49,8 +68,10 @@ function [curvature] = curvature(x, y)
     curvature = chop(numerator ./ denominator);
 end
 
-function [arcLength, totalLength] = arcLength(x, y)
+function [arcLength, totalLength] = arcLength(curve)
 % Compute cumulative arc length, then normalize length to 1
+    x = curve(1,:);
+    y = curve(2,:);
     x_prime = chop(gradient(x));
     y_prime = chop(gradient(y));
     N = length(x_prime);
